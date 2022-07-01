fa5eb24a3350b9dbfb671317116fc993a2e365cd
yongchan <aosjrl101@gmail.com>
2022-07-01 16:19:37 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week01/prac/Prac.java
@@ -0,0 +1,16 @@
+package com.sparta.week01.prac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prac {
+    public static void main(String[] args) {
+        List<String> courseList = new ArrayList<>();
+        String course1 = "웹개발 종합반";
+        String course2 = "앱개발 종합반";
+        courseList.add(course1);
+        courseList.add(course2);
+        System.out.println(courseList);
+    }
+
+ }
